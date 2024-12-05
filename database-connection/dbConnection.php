864ef16a75050ce2037b33c82865cfcae802fdaa
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-05 10:12:58 +0600

    access data in different way

--- database-connection/dbConnection.php
+++ database-connection/dbConnection.php
@@ -3,13 +3,13 @@
 // $user = "root";
 // $password ="";
 // $dbname = "db_name";
-$connection = mysqli_connect("localhost", "root", "","demo_database");
+// $connection = mysqli_connect("localhost", "root", "","demo_database");
 
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully";
-}
+// if (!$connection) {
+//     die("Connection failed: " . mysqli_connect_error());
+// } else {
+//     echo "Connected successfully";
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,9 +35,6 @@ if (!$connection) {
 <body>
 
 <h2>User Information</h2>
-<?php 
-$db = mysqli_connect("localhost","root", "","demo_database");
-?>
 <table>
     <tr>
         <th>ID</th>
@@ -45,14 +42,21 @@ $db = mysqli_connect("localhost","root", "","demo_database");
         <th>Email</th>
     </tr>
     <?php 
-    $user = $db->query("select * from student");
-    while(list($id,$name, $email) = $user->fetch_row()) {
+    $db = mysqli_connect("localhost","root", "","demo_database");
+
+    if (!$db) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+        $user = $db->query("select * from student");
+          while(list($id,$name, $email) = $user->fetch_row()) {
         echo "<tr>
                     <td>$id</td>
                     <td>$name</td>
                     <td>$email</td>
         ";
     }
+    }
+    
     ?>
 </table>
 
