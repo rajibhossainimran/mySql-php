7f3d3ebae5a58f13c08326f35b98a00f35afb82d
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-09 12:11:10 +0600

    little change

--- database-connection/update.php
+++ database-connection/update.php
@@ -39,7 +39,6 @@ if (isset($_POST["btnUpdate"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Data</title>
     <style>
-        /* Global styles */
         body {
             font-family: Arial, sans-serif;
             background-color: #f4f7fc;
